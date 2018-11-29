aa481de26d65da3d03d114bbe7fc5a3a7fb4f42f
Carlos Vializ <cvializ@vt.edu>
2018-11-29 17:47:57 -0500

    reset 2

--- spec/test-amp.js
+++ spec/test-amp.js
@@ -1,60 +1,62 @@
 import * as describes from '../lib/describes';
-import {expect} from 'chai';
+import {install} from '../lib/expect';
+import chai, {expect} from 'chai';
+import {ControllerPromise} from '../lib/controller/functional-test-controller';
+// describes.testcafe('AMP carousel', {
+//     browsers: ['chrome'],
+// }, async env => {
+//     let expect;
+//     let controller;
+//     let nextButton;
 
-describes.testcafe('AMP carousel', {
-    browsers: ['chrome'],
-}, async env => {
-    let controller;
-    let nextButton;
+//     beforeEach(async () => {
+//         controller = env.controller;
 
-    beforeEach(async () => {
-        controller = env.controller;
+//         // It is suggested that on HTML side, add proper CSS rules to make things simpler
+//         // Also, it is suggested that different carousel for testing sit on a different page
+//         await controller.navigateTo('https://static.ampb.in/c4eNlMzf6mZE1bHZVziK.html');
 
-        // It is suggested that on HTML side, add proper CSS rules to make things simpler
-        // Also, it is suggested that different carousel for testing sit on a different page
-        await controller.navigateTo('https://static.ampb.in/c4eNlMzf6mZE1bHZVziK.html');
+//         const nextButtonSelector = '#slides .amp-carousel-button.amp-carousel-button-next';
+//         nextButton = await controller.findElement(nextButtonSelector);
+//     });
 
-        const nextButtonSelector = '#slides .amp-carousel-button.amp-carousel-button-next';
-        nextButton = await controller.findElement(nextButtonSelector);
-    });
+//     it('should navigate to the next slide', async () => {
+//         await controller.click(nextButton);
 
-    it('should navigate to the next slide', async () => {
-        await controller.click(nextButton);
+//         const img1 = await controller.findElement(':first-child > amp-img');
+//         expect(await controller.getElementAttribute(img1, 'aria-hidden')).to.equal('true');
 
-        const img1 = await controller.findElement(':first-child > amp-img');
-        expect(await controller.getElementAttribute(img1, 'aria-hidden')).to.equal('true');
+//         const img2 = await controller.findElement(':nth-child(2) > amp-img');
+//         expect(await controller.getElementAttribute(img2, 'aria-hidden')).to.equal('false');
+//     });
+// });
 
-        const img2 = await controller.findElement(':nth-child(2) > amp-img');
-        expect(await controller.getElementAttribute(img2, 'aria-hidden')).to.equal('false');
-    });
-});
+install(chai);
 
 describes.selenium('AMP carousel', {
-    browsers: ['chrome'],
+  browsers: ['chrome'],
 }, async env => {
-    let controller;
-    let nextButton;
+  let controller;
+  let nextButton;
 
-    beforeEach(async () => {
-        controller = env.controller;
+  beforeEach(async () => {
+    controller = env.controller;
 
-        // It is suggested that on HTML side, add proper CSS rules to make things simpler
-        // Also, it is suggested that different carousel for testing sit on a different page
-        await controller.navigateTo('https://static.ampb.in/c4eNlMzf6mZE1bHZVziK.html');
+    // It is suggested that on HTML side, add proper CSS rules to make things simpler
+    // Also, it is suggested that different carousel for testing sit on a different page
+    await controller.navigateTo('https://static.ampb.in/c4eNlMzf6mZE1bHZVziK.html');
 
-        const nextButtonSelector = '#slides .amp-carousel-button.amp-carousel-button-next';
-        nextButton = await controller.findElement(nextButtonSelector);
-    });
+    const nextButtonSelector = '#slides .amp-carousel-button.amp-carousel-button-next';
+    nextButton = await controller.findElement(nextButtonSelector);
+  });
 
-    it('should navigate to the next slide', async () => {
-        await controller.click(nextButton);
+  it('should navigate to the next slide', async () => {
+    await controller.click(nextButton);
 
-        await controller.findElement('[title="Next item in carousel (3 of 3)"]');
+    const img1 = await controller.findElement(':first-child > amp-img');
+    await expect(controller.getElementAttribute(img1, 'aria-hidden')).to.not.equal('true');
 
-        const img1 = await controller.findElement(':first-child > amp-img');
-        expect(await controller.getElementAttribute(img1, 'aria-hidden')).to.equal('true');
-
-        const img2 = await controller.findElement(':nth-child(2) > amp-img');
-        expect(await controller.getElementAttribute(img2, 'aria-hidden')).to.equal('false');
-    });
+    const img2 = await controller.findElement(':nth-child(2) > amp-img');
+    await expect(controller.getElementAttribute(img2, 'aria-hidden')).to.not.equal('false');
+  });
 });
