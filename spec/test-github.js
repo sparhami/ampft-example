736c0968ed4b099be5ec18d8ba673e592fa5d035
Carlos Vializ <cvializ@vt.edu>
2018-12-04 12:03:34 -0500

    It's working well?

--- spec/test-github.js
+++ spec/test-github.js
@@ -1,56 +1,12 @@
 import * as describes from '../lib/describes';
-import {expect} from 'chai';
+import {expect} from '../lib/expect';
+import { ControllerPromise } from '../lib/controller/functional-test-controller';
 
 const Keys = {
     ENTER: 'enter',
 };
 
-// describes.testcafe('GitHub search results', {
-//     browsers: ['chrome'],
-// }, async env => {
-//     let controller;
-
-//     beforeEach(async () => {
-//         controller = env.controller;
-//         await controller.navigateTo('https://github.com/');
-//     });
-
-//     it('should contain a result for the search term', async () => {
-//         const searchButtonHandle = await controller.findElement('.header-search-input');
-//         await controller.type(searchButtonHandle, 'TestCafe');
-//         await controller.type(null, Keys.ENTER);
-
-//         const title = await controller.getTitle();
-//         expect(title).to.match(/TestCafe/);
-
-//         const itemHandle = await controller.findElement('.repo-list-item');
-//         const itemText = await controller.getElementText(itemHandle)
-//         expect(itemText).to.contain('DevExpress/testcafe');
-//     });
-// });
-
-// describes.testcafe('GitHub login', {
-//     browsers: ['chrome'],
-// }, async env => {
-//     let controller;
-
-//     beforeEach(async () => {
-//         controller = env.controller;
-//         await controller.navigateTo('https://github.com/login');
-//     });
-
-//     it('should fail to login with no credentials', async () => {
-//         const loginButton = await controller.findElement('.btn.btn-primary.btn-block');
-//         await controller.click(loginButton);
-
-//         const errorHandle = await controller.findElement('#js-flash-container > div > div');
-//         const errorText = await controller.getElementText(errorHandle);
-//         expect(errorText).to.contain('Incorrect username or password.');
-//     });
-// });
-
-
-describes.selenium('GitHub search results', {
+describes.testcafe('GitHub search results', {
     browsers: ['chrome'],
 }, async env => {
     let controller;
@@ -65,16 +21,14 @@ describes.selenium('GitHub search results', {
         await controller.type(searchButtonHandle, 'TestCafe');
         await controller.type(null, Keys.ENTER);
 
-        const title = await controller.getTitle();
-        expect(title).to.match(/TestCafe/);
+        await expect(controller.getTitle()).to.contain('TestCafe');
 
         const itemHandle = await controller.findElement('.repo-list-item');
-        const itemText = await controller.getElementText(itemHandle)
-        expect(itemText).to.contain('DevExpress/testcafe');
+        await expect(controller.getElementText(itemHandle)).to.contain('DevExpress/testcafe');
     });
 });
 
-describes.selenium('GitHub login', {
+describes.testcafe('GitHub login', {
     browsers: ['chrome'],
 }, async env => {
     let controller;
@@ -89,7 +43,6 @@ describes.selenium('GitHub login', {
         await controller.click(loginButton);
 
         const errorHandle = await controller.findElement('#js-flash-container > div > div');
-        const errorText = await controller.getElementText(errorHandle);
-        expect(errorText).to.contain('Incorrect username or password.');
+        await expect(controller.getElementText(errorHandle)).to.contain('Incorrect username or password.');
     });
 });
